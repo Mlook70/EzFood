e869d81e16637be33c468be9f8f5c473091459fe
Mlook70 <Abdulmalek.akel@gmail.com>
2024-05-02 22:52:11 +0300

    EzFood for getting food easily from EzTalk Owner Abdulmalek

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+const MealsSlug = ({params}) =>{
+    return(
+        <main>
+            <h1>{params.slug}</h1>
+        </main>
+    );
+};
+
+export default MealsSlug;
---
+++ app/meals/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const MealsPage = () =>{
+    return(
+        <main>
+            <h1>Meals Page !!</h1>
+
+            <Link></Link>
+        </main>
+    );
+};
+
+export default MealsPage;
